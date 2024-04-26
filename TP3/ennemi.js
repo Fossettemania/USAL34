161dcdda1ac1d69ebab977911f8f3030f96bca2d
RODRIGUEZ-DE LA NAVA Lydia <lydia.rodriguez-de-la-nava@inria.fr>
2024-04-26 14:29:05 +0200

    enlever la solution omggit rm TP3/table-de-2.js TP3/ennemi.jsgit rm TP3/table-de-2.js TP3/ennemi.js

--- TP3/ennemi.js
+++
@@ -1,9 +0,0 @@
-function bouger() {
-	console.log("Je bouge");
-	let bouton = document.getElementById("bouton");
-    var t = Math.ceil(Math.random()*(window.innerHeight-75));
-    var l = Math.ceil(Math.random()*(window.innerWidth-75));
-    bouton.style.top = t + "px";
-    bouton.style.left= l + "px";
-    bouton.style.opacity /= 2; 
-}
